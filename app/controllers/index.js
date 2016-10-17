5ba2f6f9d2e762116ce2d9ddb5f26a202797c712
Matic Jurglic <matic@jurglic.si>
2016-10-17 23:15:36 +0200

    😍

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,130 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['routing', 'virtual-dom', 'uni-flow', '2-way-binding', 'templating', 'old-browsers', 'cli',
+                'lazy', 'testing', 'stable', 'mvc', 'state', 'ssr', 'functional', 'jsx', 'native', 'full-stack'],
+
+  routing: null,
+  "virtual-dom": null,
+  "uni-flow": null,
+  "2-way-binding": null,
+  templating: null,
+  "old-browsers": null,
+  lazy: null,
+  testing: null,
+  stable: null,
+  mvc: null,
+  state: null,
+  ssr: null,
+  functional: null,
+  jsx: null,
+  "native": null,
+  "full-stack": null,
+  cli: null,
+
+  filters: [
+    {
+      name: "Routing",
+      param: "routing",
+      tooltip: ""
+    },
+    {
+      name: "Virtual DOM",
+      param: "virtual-dom",
+      tooltip: ""
+    },
+    {
+      name: "CLI tools",
+      param: "cli",
+      tooltip: "",
+    },
+    {
+      name: "Unidirectional data flow",
+      param: "uni-flow",
+      tooltip: ""
+    },
+    {
+      name: "2-way binding",
+      param: "2-way-binding",
+      tooltip: ""
+    },
+    {
+      name: "Templating engine",
+      param: "templating",
+      tooltip: ""
+    },
+    {
+      name: "Old browser support",
+      param: "old-browsers",
+      tooltip: ""
+    },
+    {
+      name: "Lazy module loading",
+      param: "lazy",
+      tooltip: ""
+    },
+    {
+      name: "Integrated testing",
+      param: "testing",
+      tooltip: ""
+    },
+    {
+      name: "Stable",
+      param: "stable",
+      tooltip: ""
+    },
+    {
+      name: "MVC",
+      param: "mvc",
+      tooltip: ""
+    },
+    {
+      name: "Integrated State management",
+      param: "state",
+      tooltip: ""
+    },
+    {
+      name: "Server side rendering",
+      param: "ssr",
+      tooltip: ""
+    },
+    {
+      name: "Functional",
+      param: "functional",
+      tooltip: ""
+    },
+    {
+      name: "JSX",
+      param: "jsx",
+      tooltip: ""
+    },
+    {
+      name: "Native",
+      param: "native",
+      tooltip: ""
+    },
+    {
+      name: "Full stack",
+      param: "full-stack",
+      tooltip: ""
+    }
+  ],
+
+  actions: {
+    clearFilters() {
+      for (let filter of this.filters) {
+        this.set(filter.param, null);
+      }
+    },
+    toggleFilter(filterName) {
+      if (this[filterName]) {
+        this.set(filterName, null);
+      } else {
+        this.set(filterName, true);
+      }
+    }
+  },
+
   frameworks: Ember.computed.alias('model'),
   collections: [
     // best communities, most jobs
@@ -20,7 +144,7 @@ export default Ember.Controller.extend({
       link: "#"
     },
     {
-      name: "Most popular",
+      name: "Most jobs",
       bgUrl: "",
       link: "#"
     },
